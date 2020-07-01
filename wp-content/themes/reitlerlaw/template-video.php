eacf50b6a81c45c6a63b3719c2da8bca3ca2106c
Vishal Sharma <vsharma@bluecorona.com>
2020-07-01 14:45:32 +0530

    Ticket #21430
    Video and Webinar - pages

---
+++ wp-content/themes/reitlerlaw/template-video.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Template Name: Video Page Template
+ */
+
+get_header();
+?>
+
+
+<main class="site-content">
+  <section class="subpage-banner">
+    <?php 
+  $banner_img = get_field('add_banner');
+  ?>
+    <?php if ($banner_img ) { ?>
+    <picture>
+      <source media="(max-width: 480px)" srcset="<?php echo esc_url($banner_img['url']); ?>" alt="<?php echo esc_attr($add_banner['alt']); ?>" />
+      <source media="(max-width: 767px)" srcset="<?php echo esc_url($banner_img['url']); ?>" alt="<?php echo esc_attr($add_banner['alt']); ?>" />
+      <img src="<?php echo esc_url($banner_img['url']); ?>" alt="<?php echo esc_attr($add_banner['alt']); ?>" />
+    </picture>
+    <?php }  else { ?>
+
+    <picture>
+      <source media="(max-width: 480px)" srcset="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/uploads/2019/11/subpage-banner.jpg" alt="Banner Image" />
+      <source media="(max-width: 767px)" srcset="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/uploads/2019/11/subpage-banner.jpg" alt="Banner Image" />
+      <img src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/uploads/2019/11/subpage-banner.jpg" alt="Banner Image" />
+    </picture>
+    <?php } ?>
+  </section>
+  <section class="subpage-content page-transection-content">
+    <div class="container">
+      <div style="padding: 25px; display: flex; flex-direction: column;">
+        <a href="/"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i> Return Home</a>
+        <br />
+        <a href="/reitler-library/"><i class="fa fa-arrow-circle-left" aria-hidden="true"></i> Return to Library</a>
+      </div>
+      <div class="partener-logo-col">
+        <h1><?php the_title() ; ?></h1>
+      </div>
+
+      <div class="library-articles-container">
+        <?php
+          $args = array(
+            'post_type'   => 'article',
+            'post_status' => 'publish',
+            'posts_per_page' => -1,
+            'orderby' => 'date',
+            'order' => 'ASC', 
+          );
+
+          $articles = new WP_Query( $args );
+
+          if ( $articles->have_posts() ) :
+        ?>
+        <?php while ( $articles->have_posts() ) : $articles->the_post(); ?>
+        <?php if ( get_field('article_category') == 'Video' ) { ?> 
+          <div class="article-box">
+            <a class="article-text" style="font-size: 24px;" href="<?php the_permalink() ?>"><?php the_title() ?><a>
+          </div>
+        <?php } ?>
+        <?php endwhile; ?>
+        <?php wp_reset_postdata(); ?>  
+        <?php endif; ?>
+      </div>
+
+    </div>
+
+    <div class="repres-extra-sec">
+      <?php if($count > 5){  ?>
+      <div class="transection-extra-col transection-bt section-1 as-sw-hd"><i class="far fa-minus-circle"></i><i class="far fa-plus-circle"></i><span>View More</span></div>
+      <?php } ?>
+      <div class="transection-extra-col transection-form"><span><i class="far fa-search"></i> Search</span>
+        <form class="transection-search" action="<?php echo esc_url( home_url( '/' ) ); ?>" method="get">
+          <input type="text" name="s" id="search" class="search-text">
+          <input type="image" src="<?php echo esc_url( home_url( '/' ) ); ?>wp-content/themes/reitlerlaw/assets/images/search-icon.png" class="submit-icon">
+        </form>
+
+      </div>
+    </div>
+
+    </div>
+    </div>
+  </section>
+</main>
+<?php
+get_footer();
